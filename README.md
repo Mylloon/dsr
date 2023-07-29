1d01f8b0a97edd9ed9fb22eea991fd3ce3a14415
Mylloon <kennel.anri@tutanota.com>
2023-07-29 14:29:14 +0200

    change packager, following up the starting guide

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Discord Video Sharing
 
-- [ ] Try to keep the video under 25mn <!-- Use Handbrake preset? -->
+- [ ] Try to keep the video under 25mb <!-- Use Handbrake preset? -->
   - [ ] If already under the limit, the file won't be compressed
 - [ ] Merge all audio files into one track <!-- ffmpeg -i in.mp4 -filter_complex "[0:a]amerge=inputs=2[a]" -ac 1 -map 0:v -map "[a]" -c:v copy out.mp4 -->
+
+## Dev
+
+```bash
+npm install --platform=win32
+npm run package
+```
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,26 @@
 import { app, BrowserWindow } from "electron";
-
-const pathToFfmpeg = require("ffmpeg-static");
+import path = require("path");
+/* import ffmpegPath = require("ffmpeg-static"); */
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
 
   win.loadFile("../index.html");
 };
-
 app.whenReady().then(() => {
-  /* console.log(pathToFfmpeg); */
   createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
 });

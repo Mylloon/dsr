9015029fcfc403de8a3e6391ea5583a4d970dca3
Mylloon <kennel.anri@tutanota.com>
2023-08-01 15:14:04 +0200

    fix path issue and make fn async

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -10,9 +10,11 @@ export const getNewFilename = (ogFile: string, part: string) => {
 };
 
 /** Return the duration of a video in second */
-export const getVideoDuration = (file: string) => {
-  const command = `${ffprobe.path} -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "${file}"`;
-  const durationString = child_process.execSync(command).toString().trim();
+export const getVideoDuration = async (file: string) => {
+  const command = `"${ffprobe.path}" -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "${file}"`;
+  const durationString = await execute(command).then((output) =>
+    output.toString().trim()
+  );
   return parseFloat(durationString);
 };
 

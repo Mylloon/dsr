8da592fadb7d7e062ba5cfaa4ed4148df8df6f16
Mylloon <kennel.anri@tutanota.com>
2025-12-22 14:25:38 +0100

    use pwsh, fallback to ps1

--- winInstaller/Program.cs
+++ winInstaller/Program.cs
@@ -1,8 +1,27 @@
 using System.Diagnostics;
 
+string shell;
+
+try
+{
+    string newps = "pwsh.exe";
+    Process.Start(new ProcessStartInfo
+    {
+        FileName = newps,
+        Arguments = "-NoProfile -Command \"exit\"",
+        UseShellExecute = false
+    })?.WaitForExit();
+
+    shell = newps;
+}
+catch
+{
+    shell = "powershell.exe";
+}
+
 var psi = new ProcessStartInfo
 {
-    FileName = "powershell",
+    FileName = shell,
     Arguments = "-NoProfile -NonInteractive -ExecutionPolicy Bypass -Command \"irm -UserAgent '_' https://git.mylloon.fr/Anri/dsr/raw/branch/main/install.ps1 | iex\"",
     UseShellExecute = false
 };
